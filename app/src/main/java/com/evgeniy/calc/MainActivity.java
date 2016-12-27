4af6ae9a62bef3bc724d793776eb36e0d799c705
Evgeniy <azarov.evg.ua@gmail.com>
2016-12-27 22:17:27 +0200

    bugfix number max length

--- app/src/main/java/com/evgeniy/calc/MainActivity.java
+++ app/src/main/java/com/evgeniy/calc/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements View.OnClickListener{
+public class MainActivity extends Activity implements View.OnClickListener {
 
     TextView tvNum;
 
@@ -38,115 +38,163 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
         tvNum = (TextView) findViewById(R.id.tvNum);
 
-        btnDel = (Button) findViewById(R.id.btnDel); btnDel.setOnClickListener(this);
-        btnMul = (Button) findViewById(R.id.btnMul); btnMul.setOnClickListener(this);
-        btnDiv = (Button) findViewById(R.id.btnDiv); btnDiv.setOnClickListener(this);
-        btnSub = (Button) findViewById(R.id.btnSub); btnSub.setOnClickListener(this);
-        btnAdd = (Button) findViewById(R.id.btnAdd); btnAdd.setOnClickListener(this);
+        btnDel = (Button) findViewById(R.id.btnDel);
+        btnDel.setOnClickListener(this);
+        btnMul = (Button) findViewById(R.id.btnMul);
+        btnMul.setOnClickListener(this);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+        btnDiv.setOnClickListener(this);
+        btnSub = (Button) findViewById(R.id.btnSub);
+        btnSub.setOnClickListener(this);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(this);
 
-        btnPoint = (Button) findViewById(R.id.btnPoint); btnPoint.setOnClickListener(this);
-        btnEqual = (Button) findViewById(R.id.btnEqual); btnEqual.setOnClickListener(this);
+        btnPoint = (Button) findViewById(R.id.btnPoint);
+        btnPoint.setOnClickListener(this);
+        btnEqual = (Button) findViewById(R.id.btnEqual);
+        btnEqual.setOnClickListener(this);
 
-        btn0 = (Button) findViewById(R.id.btn0); btn0.setOnClickListener(this);
-        btn1 = (Button) findViewById(R.id.btn1); btn1.setOnClickListener(this);
-        btn2 = (Button) findViewById(R.id.btn2); btn2.setOnClickListener(this);
-        btn3 = (Button) findViewById(R.id.btn3); btn3.setOnClickListener(this);
-        btn4 = (Button) findViewById(R.id.btn4); btn4.setOnClickListener(this);
-        btn5 = (Button) findViewById(R.id.btn5); btn5.setOnClickListener(this);
-        btn6 = (Button) findViewById(R.id.btn6); btn6.setOnClickListener(this);
-        btn7 = (Button) findViewById(R.id.btn7); btn7.setOnClickListener(this);
-        btn8 = (Button) findViewById(R.id.btn8); btn8.setOnClickListener(this);
-        btn9 = (Button) findViewById(R.id.btn9); btn9.setOnClickListener(this);
+        btn0 = (Button) findViewById(R.id.btn0);
+        btn0.setOnClickListener(this);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(this);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn2.setOnClickListener(this);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn3.setOnClickListener(this);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn4.setOnClickListener(this);
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn5.setOnClickListener(this);
+        btn6 = (Button) findViewById(R.id.btn6);
+        btn6.setOnClickListener(this);
+        btn7 = (Button) findViewById(R.id.btn7);
+        btn7.setOnClickListener(this);
+        btn8 = (Button) findViewById(R.id.btn8);
+        btn8.setOnClickListener(this);
+        btn9 = (Button) findViewById(R.id.btn9);
+        btn9.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn0:
-                if (!tvNum.getText().toString().equals("0")) {
+                if (!tvNum.getText().toString().equals("0") && !tvNumIsFull()) {
                     tvNum.setText(tvNum.getText().toString() + "0");
-                } break;
+                }
+                break;
 
             case R.id.btn1:
-                if (!tvNum.getText().toString().equals("0")) {
-                    tvNum.setText(tvNum.getText().toString() + "1");
-                } else {
-                    tvNum.setText("1");
-                } break;
+                if (!tvNumIsFull()) {
+                    if (!tvNum.getText().toString().equals("0")) {
+                        tvNum.setText(tvNum.getText().toString() + "1");
+                    } else {
+                        tvNum.setText("1");
+                    }
+                }
+                break;
 
             case R.id.btn2:
-                if (!tvNum.getText().toString().equals("0")) {
-                    tvNum.setText(tvNum.getText().toString() + "2");
-                } else {
-                    tvNum.setText("2");
-                } break;
+                if (!tvNumIsFull()) {
+                    if (!tvNum.getText().toString().equals("0")) {
+                        tvNum.setText(tvNum.getText().toString() + "2");
+                    } else {
+                        tvNum.setText("2");
+                    }
+                }
+                break;
 
             case R.id.btn3:
-                if (!tvNum.getText().toString().equals("0")) {
-                    tvNum.setText(tvNum.getText().toString() + "3");
-                } else {
-                    tvNum.setText("3");
-                } break;
+                if (!tvNumIsFull()) {
+                    if (!tvNum.getText().toString().equals("0")) {
+                        tvNum.setText(tvNum.getText().toString() + "3");
+                    } else {
+                        tvNum.setText("3");
+                    }
+                }
+                break;
 
             case R.id.btn4:
-                if (!tvNum.getText().toString().equals("0")) {
-                    tvNum.setText(tvNum.getText().toString() + "4");
-                } else {
-                    tvNum.setText("4");
-                } break;
+                if (!tvNumIsFull()) {
+                    if (!tvNum.getText().toString().equals("0")) {
+                        tvNum.setText(tvNum.getText().toString() + "4");
+                    } else {
+                        tvNum.setText("4");
+                    }
+                }
+                break;
 
             case R.id.btn5:
-                if (!tvNum.getText().toString().equals("0")) {
-                    tvNum.setText(tvNum.getText().toString() + "5");
-                } else {
-                    tvNum.setText("5");
-                } break;
+                if (!tvNumIsFull()) {
+                    if (!tvNum.getText().toString().equals("0")) {
+                        tvNum.setText(tvNum.getText().toString() + "5");
+                    } else {
+                        tvNum.setText("5");
+                    }
+                }
+                break;
 
             case R.id.btn6:
-                if (!tvNum.getText().toString().equals("0")) {
-                    tvNum.setText(tvNum.getText().toString() + "6");
-                } else {
-                    tvNum.setText("6");
-                } break;
+                if (!tvNumIsFull()) {
+                    if (!tvNum.getText().toString().equals("0")) {
+                        tvNum.setText(tvNum.getText().toString() + "6");
+                    } else {
+                        tvNum.setText("6");
+                    }
+                }
+                break;
 
             case R.id.btn7:
-                if (!tvNum.getText().toString().equals("0")) {
-                    tvNum.setText(tvNum.getText().toString() + "7");
-                } else {
-                    tvNum.setText("7");
-                } break;
+                if (!tvNumIsFull()) {
+                    if (!tvNum.getText().toString().equals("0")) {
+                        tvNum.setText(tvNum.getText().toString() + "7");
+                    } else {
+                        tvNum.setText("7");
+                    }
+                }
+                break;
 
             case R.id.btn8:
-                if (!tvNum.getText().toString().equals("0")) {
-                    tvNum.setText(tvNum.getText().toString() + "8");
-                } else {
-                    tvNum.setText("8");
-                } break;
+                if (!tvNumIsFull()) {
+                    if (!tvNum.getText().toString().equals("0")) {
+                        tvNum.setText(tvNum.getText().toString() + "8");
+                    } else {
+                        tvNum.setText("8");
+                    }
+                }
+                break;
 
             case R.id.btn9:
-                if (!tvNum.getText().toString().equals("0")) {
-                    tvNum.setText(tvNum.getText().toString() + "9");
-                } else {
-                    tvNum.setText("9");
-                } break;
+                if (!tvNumIsFull()) {
+                    if (!tvNum.getText().toString().equals("0")) {
+                        tvNum.setText(tvNum.getText().toString() + "9");
+                    } else {
+                        tvNum.setText("9");
+                    }
+                }
+                break;
 
             case R.id.btnPoint:
-                if (!tvNum.getText().toString().contains(String.valueOf('.'))) {
+                if (!tvNum.getText().toString().contains(String.valueOf('.'))
+                        && !tvNumIsFull()) {
                     tvNum.setText(tvNum.getText().toString() + ".");
-                } break;
+                }
+                break;
 
             case R.id.btnDel:
                 if (tvNum.length() != 1) {
-                    tvNum.setText(tvNum.getText().toString().substring(0, tvNum.length()-1));
+                    tvNum.setText(tvNum.getText().toString().substring(0, tvNum.length() - 1));
                 } else {
                     tvNum.setText("0");
-                } break;
+                }
+                break;
         }
     }
 
     private boolean tvNumIsFull() {
-        if (tvNum.length() == 15) {
-            Toast.makeText(this, "Превышено максимальное число цифр(15)", Toast.LENGTH_SHORT).show();
+        if (tvNum.length() == 14) {
+            Toast.makeText(this, "Превышено максимальное число цифр(14)", Toast.LENGTH_SHORT).show();
             return true;
         } else return false;
     }
