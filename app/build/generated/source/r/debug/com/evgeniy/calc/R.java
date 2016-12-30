baea4ae4afa76c078b7cf0a50e99e1a0d45f97b9
Evgeniy <azarov.evg.ua@gmail.com>
2016-12-30 17:44:57 +0200

    new interface

--- app/build/generated/source/r/debug/com/evgeniy/calc/R.java
+++ app/build/generated/source/r/debug/com/evgeniy/calc/R.java
@@ -11,71 +11,76 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int colorAccent=0x7f060000;
-        public static final int colorPrimary=0x7f060001;
-        public static final int colorPrimaryDark=0x7f060002;
+        public static final int colorAccent=0x7f070000;
+        public static final int colorPrimary=0x7f070001;
+        public static final int colorPrimaryDark=0x7f070002;
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
+    }
+    public static final class drawable {
+        public static final int circle_button=0x7f020000;
+        public static final int circle_button_normal=0x7f020001;
+        public static final int circle_button_pressed=0x7f020002;
     }
     public static final class id {
-        public static final int action_about=0x7f090015;
-        public static final int action_settings=0x7f090014;
-        public static final int activity_main=0x7f090000;
-        public static final int btn0=0x7f090002;
-        public static final int btn1=0x7f090006;
-        public static final int btn2=0x7f090005;
-        public static final int btn3=0x7f090004;
-        public static final int btn4=0x7f090009;
-        public static final int btn5=0x7f090012;
-        public static final int btn6=0x7f090008;
-        public static final int btn7=0x7f09000d;
-        public static final int btn8=0x7f09000c;
-        public static final int btn9=0x7f09000b;
-        public static final int btnAdd=0x7f090007;
-        public static final int btnDel=0x7f09000e;
-        public static final int btnDiv=0x7f09000f;
-        public static final int btnEqual=0x7f090003;
-        public static final int btnMul=0x7f090010;
-        public static final int btnPlusMinus=0x7f090011;
-        public static final int btnPoint=0x7f090001;
-        public static final int btnSub=0x7f09000a;
-        public static final int etNum=0x7f090013;
+        public static final int action_about=0x7f0a0015;
+        public static final int action_settings=0x7f0a0014;
+        public static final int activity_main=0x7f0a0000;
+        public static final int btn0=0x7f0a0002;
+        public static final int btn1=0x7f0a0006;
+        public static final int btn2=0x7f0a0005;
+        public static final int btn3=0x7f0a0004;
+        public static final int btn4=0x7f0a0009;
+        public static final int btn5=0x7f0a0012;
+        public static final int btn6=0x7f0a0008;
+        public static final int btn7=0x7f0a000d;
+        public static final int btn8=0x7f0a000c;
+        public static final int btn9=0x7f0a000b;
+        public static final int btnAdd=0x7f0a0007;
+        public static final int btnDel=0x7f0a000e;
+        public static final int btnDiv=0x7f0a000f;
+        public static final int btnEqual=0x7f0a0003;
+        public static final int btnMul=0x7f0a0010;
+        public static final int btnPlusMinus=0x7f0a0011;
+        public static final int btnPoint=0x7f0a0001;
+        public static final int btnSub=0x7f0a000a;
+        public static final int etNum=0x7f0a0013;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_main=0x7f040000;
     }
     public static final class menu {
-        public static final int menu_main=0x7f080000;
+        public static final int menu_main=0x7f090000;
     }
     public static final class mipmap {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_launcher=0x7f030000;
     }
     public static final class string {
-        public static final int action_about=0x7f040000;
-        public static final int action_settings=0x7f040001;
-        public static final int app_name=0x7f040002;
-        public static final int button_0=0x7f040003;
-        public static final int button_1=0x7f040004;
-        public static final int button_2=0x7f040005;
-        public static final int button_3=0x7f040006;
-        public static final int button_4=0x7f040007;
-        public static final int button_5=0x7f040008;
-        public static final int button_6=0x7f040009;
-        public static final int button_7=0x7f04000a;
-        public static final int button_8=0x7f04000b;
-        public static final int button_9=0x7f04000c;
-        public static final int button_add=0x7f04000d;
-        public static final int button_delete=0x7f04000e;
-        public static final int button_division=0x7f04000f;
-        public static final int button_equal=0x7f040010;
-        public static final int button_multiple=0x7f040011;
-        public static final int button_plus_minus=0x7f040012;
-        public static final int button_point=0x7f040013;
-        public static final int button_subtract=0x7f040014;
+        public static final int action_about=0x7f050000;
+        public static final int action_settings=0x7f050001;
+        public static final int app_name=0x7f050002;
+        public static final int button_0=0x7f050003;
+        public static final int button_1=0x7f050004;
+        public static final int button_2=0x7f050005;
+        public static final int button_3=0x7f050006;
+        public static final int button_4=0x7f050007;
+        public static final int button_5=0x7f050008;
+        public static final int button_6=0x7f050009;
+        public static final int button_7=0x7f05000a;
+        public static final int button_8=0x7f05000b;
+        public static final int button_9=0x7f05000c;
+        public static final int button_add=0x7f05000d;
+        public static final int button_delete=0x7f05000e;
+        public static final int button_division=0x7f05000f;
+        public static final int button_equal=0x7f050010;
+        public static final int button_multiple=0x7f050011;
+        public static final int button_plus_minus=0x7f050012;
+        public static final int button_point=0x7f050013;
+        public static final int button_subtract=0x7f050014;
     }
     public static final class style {
-        public static final int AppTheme=0x7f070000;
+        public static final int AppTheme=0x7f080000;
     }
 }
